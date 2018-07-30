44d0f34c8e2e298f1f66660f5ac8f893cfaf94a4
Kevin Chi <kevinchi118@gmail.com>
2018-07-30 17:16:27 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,5 +2,18 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Kevin"
+  end
+
+  get '/hometown' do
+    "My hometown is NYC"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is The A Team"
 end
